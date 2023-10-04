bc621221aa90a94a39291ff93c623943e961a030
Jaime Cárdenas <jaime.cardenas176@gmail.com>
2023-10-04 11:05:53 +0200

    mas cosillas

--- src/App.java
+++ src/App.java
@@ -1,31 +1,99 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class App {
-    
-    //proceso padre
+
+    // proceso padre
     public static void main(String[] args) throws Exception {
-
+        // TODO
+        // metodo privado que imprime tus datos --> linea 48 (Ctrl + click para ir a la
+        // implementacion del metodo)
         imprimirCabecera();
+        imprimirMenu();
+        String opcion;
 
-        
+        Scanner scanner = new Scanner(System.in);
+        opcion = scanner.nextLine();
 
+        while (opcion != "0") {
+            //TODO caso 0 y 2 no estan implementados
+            //TODO solo funciona el caso 1, asi que no pruebes otra cosa
+            switch (opcion) {
+                case "1":
+                    try {
+                        String line;
+                        // TODO
+                        // aqui construimos el proceso hijo, le pasmaos el comando java y el archivo
+                        // .java a ejecutar
+                        // si te das cuenta he eliminado el flag "-jar" que le indica otra extension
+                        // creo que asi debe funcionar
+
+                        Process hijo = new ProcessBuilder("javac", "SubProcesoTablas.java", "java", "SubProcesoTablas.java").start();
+
+                        //funcion que me he creado para debuggear
+                        imprimeProcess(hijo);
+
+                        // leemos la entrada del padre
+                        BufferedReader entradaPadre = new BufferedReader(new InputStreamReader(System.in));
+                        imprimeProcess(hijo);
+                        // Aqui estamos creando un Objeto para Leer la salida del hijo
+                        BufferedReader entradaHijo = new BufferedReader(new InputStreamReader(hijo.getInputStream()));
+                        imprimeProcess(hijo);
+                        // Aqui estamos creando un objeto que nos permite
+                        // pintar en la salida del padre lo que nos llega directamente del proceso hijo
+                        PrintStream salidaHijo = new PrintStream(hijo.getOutputStream());
+                        imprimeProcess(hijo);
+
+                        while (!(line = entradaPadre.readLine()).equals("fin")) {
+                            imprimeProcess(hijo);
+                            salidaHijo.println(line);
+                            salidaHijo.flush();
+                            imprimeProcess(hijo);
+                            System.out.println("Mensaje enviado al proceso hijo");
+                            if ( entradaHijo.readLine() != null) {
+                                System.out.println("Resultado del proceso hijo:");
+                                imprimeProcess(hijo);
+                                System.out.println("l2 "+line);
+                            }
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Error durante la ejecución: " + e.getMessage());
+                    }
+
+                    break;
+            }
+
+        }
+        scanner.close();
+        System.out.println("Proceso finalizado");
     }
 
-
-
+    // statico para poder usarse dento del main
+    // y void por que no devuelve nada SOLO IMPRIME
     private static void imprimirCabecera() {
-           System.out.println("******************************************\r\n" +
-                "\r\n" +
-                "* PSP - Tarea Individual 1             *\r\n" +
-                "\r\n" +
-                "******************************************\r\n" +
-                "\r\n" +
-                "* Miriam Gallardo González-Amor *\r\n" +
-                "\r\n" +
-                "******************************************\r\n" +
-                "\r\n" +
-                "* DNI del alumno                          *");
+        System.out.println(
+                "******************************************\r\n\r\n"
+                        + "* PSP - Tarea Individual 1             *\r\n\r\n"
+                        + "******************************************\r\n\r\n"
+                        + "* Miriam Gallardo González-Amor *\r\n\r\n"
+                        + "******************************************\r\n\r\n"
+                        + "* DNI del alumno           *");
+    }
+
+    private static void imprimirMenu() {
+        System.out.println("Menu de Opciones");
+        System.out.println("\t 0: salir");
+        System.out.println("\t 1: ver la tabla del numero que elija");
+        System.out.println("\t 2: ver todas las tablas");
+    }
+
+    private static void imprimeProcess(Process process) {
+        System.out.println();
+        System.out.println("el proceso esta vivo? "+process.isAlive());
+        System.out.println("el proceso completo -> "+process.toString());
+        System.out.println("informacion del proceso: "+process.info());
+        System.out.println();
     }
 }
--- src/SubProcesoTablas.java
+++ src/SubProcesoTablas.java
@@ -6,25 +6,34 @@ public class SubProcesoTablas {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
+        System.out.println("ESTOY LEYENDO EN HIJO");
         String linea = bufferedReader.readLine();
-        if (linea != "") {
-            try {
-                calcularTabla(Integer.parseInt(linea));
-            } catch (Exception exception) {
-                System.out.println(exception.getMessage());
+        while (linea != "exit") {
+            if (linea != null) {
+                try {
+                    calcularTabla(Integer.parseInt(linea));
+                } catch (Exception exception) {
+                    System.out.println(exception.getMessage());
+                }
+            } else {
+                System.out.println("ESTOY VACIO");
+                calcularTodas();
             }
-        }else{
-            calcularTodas();
         }
     }
 
     private static void calcularTabla(Integer numero) throws Exception {
         if (numero > 0 && numero <= 10) {
+            System.out.println("\s\s\s\s\s\s/************* Tabla del " + numero + " *************/\n");
             for (int contador = 1; contador <= 10; contador++) {
                 int resultado = numero * contador;
-                System.out.println(numero + " x " + contador + " = " + resultado);
+                //condicional ternario (u operador ternario enn java, cambialo si quieres por una estructura if/else)
+                String lineaImpresa = (contador == 10)
+                        ? "\t\t" + numero + "\s\s\s x \s\s" + contador + "\s\s\s = \s\s\s" + resultado
+                        : "\t\t" + numero + "\s\s\s x \s\s\s" + contador + "\s\s\s = \s\s\s" + resultado;
+                System.out.println(lineaImpresa);
             }
+            System.out.println("\n\t\s\s\s\s\s\s\s\s/*******************/\n\n");
         } else {
             throw new Exception("Error, has introducido un numero fuera de rango, rango permitido [1-10]");
         }
